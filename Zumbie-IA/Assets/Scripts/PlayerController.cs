e2fb6689626cb735513d8410dede5144344ad94b
Matheus Moura <matheus.moura@prodeaf.net>
2017-09-05 02:34:51 -0300

    feat: Movimentação do player ajustada

--- Zumbie-IA/Assets/Scripts/PlayerController.cs
+++ Zumbie-IA/Assets/Scripts/PlayerController.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController:MonoBehaviour {
 
 	public Animator myAnimator;
+	public float moveSpeed = 3;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start() {
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update() {
+		transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
 		myAnimator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
-		myAnimator.SetFloat("HSpeed", Input.GetAxis("Horizontal"));
+		//myAnimator.SetFloat("HSpeed", Input.GetAxis("Horizontal"));
 	}
 }
