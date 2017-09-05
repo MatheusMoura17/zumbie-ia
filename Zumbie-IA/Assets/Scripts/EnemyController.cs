317cf80c88268b1736868c0e3de01695ce0061ef
Matheus Moura <matheus.moura@prodeaf.net>
2017-09-05 13:18:39 -0300

    feat: Controlador do inimigo adicionado

---
+++ Zumbie-IA/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController:CharacterBase {
+
+	public Transform target;
+
+	private NavMeshAgent agent;
+	private Rigidbody myRigdbody;
+
+	private bool isRunning;
+
+	private void Start() {
+		agent = GetComponent<NavMeshAgent>();
+		agent.autoBraking = true;
+	}
+
+	public override void OnKilled() {
+		//atualizar hud
+	}
+
+	public override void OnReceiveHit() {
+		//atualizar hud
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+
+		if (!isKilled) {
+			agent.destination = target.position;
+			Debug.Log(agent.velocity);
+			isRunning = agent.velocity!=Vector3.zero;
+			SetRunningAnimation(isRunning, 0);
+		}
+	}
+}
