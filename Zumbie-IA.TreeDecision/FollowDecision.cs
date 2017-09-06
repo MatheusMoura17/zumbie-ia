943ddad87efd47ba2129c88eba5aba33e19be07c
Matheus Moura <matheus.moura@prodeaf.net>
2017-09-05 21:30:37 -0300

    feat: FollowDecision.cs adicionado

---
+++ Zumbie-IA.TreeDecision/FollowDecision.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accord;
+
+namespace Zumbie_IA.TreeDecision {
+	public class FollowDecision {
+		public FollowDecision() {
+
+		}
+	}
+}
