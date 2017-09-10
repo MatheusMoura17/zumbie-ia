cd25086473ab7dca2ddb5a9e7e5f4c2f30d77dca
Matheus Moura <matheus.moura@prodeaf.net>
2017-09-10 20:20:57 -0300

    feat: Script base de AI adicionado

---
+++ Zumbie-IA/Assets/Scripts/AI/FollowDecision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowDecision : MonoBehaviour {
+
+	public string fileName = "followDecision.xls";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
